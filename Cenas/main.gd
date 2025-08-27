ad0e93251d2f2e5db1c37bbc4e5b88f60edee998
RenanRabelo1 <renanrabelo82@gmail.com>
2025-08-27 14:19:55 -0300

    Preparacao para Carros Invertidos

--- Cenas/main.gd
+++ Cenas/main.gd
@@ -4,7 +4,11 @@ var cena_carros = preload("res://Cenas/carros.tscn")
 var pistas_rapidas_y = [104, 272, 488]
 var pistas_lentas_y = [160, 216, 324, 384, 438, 544, 600]
 var score = 0 
+# Pistas para carros da ESQUERDA → DIREITA
+var pistas_esquerda_direita_y = [104, 272, 488]
 
+# Pistas para carros da DIREITA → ESQUERDA  
+var pistas_direita_esquerda_y = [160, 216, 324, 384, 438, 544, 600]
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$HUD/Placar.text = str(score)
@@ -16,21 +20,23 @@ func _ready() -> void:
 
 
 
-
 func _on_timer_carros_rapidos_timeout() -> void:
 	var carro = cena_carros.instantiate()
 	add_child(carro)
-	var pista_y = pistas_rapidas_y[randi_range(0, pistas_rapidas_y.size() - 1)]
+	# Só usa pistas da ESQUERDA → DIREITA
+	var pista_y = pistas_esquerda_direita_y[randi_range(0, pistas_esquerda_direita_y.size() - 1)]
 	carro.position = Vector2(-10, pista_y)
-	carro.set_linear_velocity(Vector2(randf_range(700.0, 720.0), 0))
+	carro.set_linear_velocity(Vector2(randf_range(900.0, 920.0), 0))  # Mais rápido!
 	carro.set_linear_damp(0.0)
 
 func _on_timer_carros_lentos_timeout() -> void:
 	var carro = cena_carros.instantiate()
 	add_child(carro)
-	var pista_y = pistas_lentas_y[randi_range(0, pistas_lentas_y.size() - 1)]
-	carro.position = Vector2(-10, pista_y)
-	carro.set_linear_velocity(Vector2(randf_range(700.0, 720.0), 0))
+	# Só usa pistas da DIREITA → ESQUERDA
+	var pista_y = pistas_direita_esquerda_y[randi_range(0, pistas_direita_esquerda_y.size() - 1)]
+	carro.position = Vector2(1350, pista_y)
+	carro.get_node("Animacao").scale.x = -1
+	carro.set_linear_velocity(Vector2(-randf_range(700.0, 720.0), 0))  # Mais lento
 	carro.set_linear_damp(0.0)
 	
 
